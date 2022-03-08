0093fd673136ceef1e487f65309e1829e66e6202
Sanchit Kumar <sk7374@nyu.edu>
2022-03-08 12:59:29 -0500

    Saving work

--- simpleNet.js
+++ simpleNet.js
@@ -1,14 +1,35 @@
 const net = require('net');
+
+class Request {
+    constructor(s) {
+        const requestParts = s.split(' ');
+        const path = requestParts[1];
+        this.path = path;
+    }
+}
+
+
 const server = net.createServer(function(sock) {
     console.log('Got connection from (addr, port):', sock.remoteAddress, sock.remotePort);
     
-    sock.on('data', function(binaryData) {
-        console.log('got data\n=====\n' + binaryData); 
-        sock.write(binaryData);
+    // sock.on('data', function(binaryData) {
+    //     console.log('got data\n=====\n' + binaryData); 
+    //     sock.write(binaryData);
     
-        // uncomment me if you want the connection to close
-        // immediately after we send back data
-        // sock.end();
+    //     // uncomment me if you want the connection to close
+    //     // immediately after we send back data
+    //     // sock.end();
+    // });
+
+    sock.on('data', (binaryData) => {
+        const reqString = '' + binaryData; 
+        const req = new Request(reqString);
+        if(req.path === '/about') {
+            sock.write('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h2>hello</h2>');
+        } else  if(req.path === '/test') {
+            sock.write('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h2>test</h2>');
+        }         
+        sock.end();
     });
 
 });
